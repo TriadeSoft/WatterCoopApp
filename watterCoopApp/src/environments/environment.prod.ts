d54a8c11ca513e0059d0b65b86d36d126807ebe2
Santiago Bergerat <santiagobergerat@gmail.com>
2025-08-15 14:20:49 -0300

    added some changs

--- watterCoopApp/src/environments/environment.prod.ts
+++ watterCoopApp/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  emailJsPublicKey: process.env['NG_APP_EMAILJS_PUBLIC_KEY'] || '',
-  emailJsServiceId: process.env['NG_APP_EMAILJS_SERVICE_ID'] || '',
-  emailJsTemplateId: process.env['NG_APP_EMAILJS_TEMPLATE_ID'] || '',
-  supabaseUrl: process.env['NG_APP_SUPABASE_URL'] || '',
-  supabaseAnonKey: process.env['NG_APP_SUPABASE_ANON_KEY'] || ''
+  emailJsPublicKey: process.env['NG_APP_EMAILJS_PUBLIC_KEY'] as string,
+  emailJsServiceId: process.env['NG_APP_EMAILJS_SERVICE_ID'] as string,
+  emailJsTemplateId: process.env['NG_APP_EMAILJS_TEMPLATE_ID'] as string,
+  supabaseUrl: process.env['NG_APP_SUPABASE_URL'] as string,
+  supabaseAnonKey: process.env['NG_APP_SUPABASE_ANON_KEY'] as string
 };
